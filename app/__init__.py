7c940ecaca4af89d160e086c87e2209df19da045
bjornum <bjoern-einar2@hotmail.no>
2025-05-18 20:49:38 +0200

    Basic routing
    Added basic home route and Jinja2 templating

--- app/__init__.py
+++ app/__init__.py
@@ -1,1 +1,11 @@
-# Flask application factory
+from flask import Flask
+
+def create_app():
+    app = Flask(__name__)
+    app.config['SECRET_KEY'] = 'dev'  # Needed for forms
+    app.config['DATABASE'] = 'sqlite:///blog.db'
+
+    from . import routes
+    app.register_blueprint(routes.bp)
+
+    return app
--- app/routes.py
+++ app/routes.py
@@ -1,1 +1,7 @@
-# Routes / Views
+from flask import Blueprint, render_template
+
+bp = Blueprint('main', __name__)
+
+@bp.route('/')
+def home():
+    return render_template('home.html')
